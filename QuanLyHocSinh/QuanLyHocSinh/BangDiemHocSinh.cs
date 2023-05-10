688207aead8b76504a7231768e827b834b862c07
phanvy <phanvy5403@gmail.com>
2023-05-10 13:29:12 +0700

    update BangDiemHocSinh form

--- QuanLyHocSinh/QuanLyHocSinh/BangDiemHocSinh.cs
+++ QuanLyHocSinh/QuanLyHocSinh/BangDiemHocSinh.cs
@@ -15,6 +15,27 @@ namespace QuanLyHocSinh
         public BangDiemHocSinh()
         {
             InitializeComponent();
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+        }
+
+        void LoadData()
+        {
+            dataEntities dtb = new dataEntities();
+            var result = from c in dtb.DIEMs
+                         where c.MaHocSinh == textBox1.Text && c.HocKy == textBox2.Text && c.NamHoc == textBox3.Text
+                         select new {c.MaMonHoc,c.MONHOC.TenMonHoc, c.DiemTX, c.DiemGK, c.DiemCK, c.DiemTB};
+            dataGridView1.DataSource = result.ToList();
+        }
+
+        private void TraCuuButton_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
