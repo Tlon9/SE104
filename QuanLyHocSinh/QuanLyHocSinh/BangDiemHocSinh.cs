26dc1f3039d41e07d91479e4d5925a62d98dab0f
phanvy <phanvy5403@gmail.com>
2023-05-11 09:54:47 +0700

    Update BDHS form

--- QuanLyHocSinh/QuanLyHocSinh/BangDiemHocSinh.cs
+++ QuanLyHocSinh/QuanLyHocSinh/BangDiemHocSinh.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using QuanLyHocSinh.Model;
+//using QuanLyHocSinh.Model;
 
 namespace QuanLyHocSinh
 {
