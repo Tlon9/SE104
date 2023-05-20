03c5bbdb6212ac3c9ec5ea1e8e10c47e965b461e
PamumanTan <manhtanhoaphu@gmail.com>
2023-05-20 11:50:46 +0700

    Update QuanLyHocSinh.sql

--- QuanLyHocSinh/QuanLyHocSinh/QuanLyHocSinh.sql
+++ QuanLyHocSinh/QuanLyHocSinh/QuanLyHocSinh.sql
@@ -29,76 +29,76 @@ CREATE TABLE HOCSINH
 )
 
 CREATE TABLE KHOI	
-(	MaKhoi nvarchar(10) not null,
-	TenKhoi nvarchar(10),
+(	MaKhoi nvarchar(20) not null,
+	TenKhoi nvarchar(30),
 	constraint pk_mk primary key(MaKhoi)
 )
 
 CREATE TABLE NAMHOC (
-	MaNamHoc nvarchar(10) not null,
-	NamHoc nvarchar(10),
+	MaNamHoc nvarchar(20) not null,
+	NamHoc varchar(9),
 	constraint pk_mnh primary key(MaNamHoc)
 )
 
 CREATE TABLE LOP
-(	MaLop nvarchar(10) not null,
-	TenLop nvarchar(10),
-	MaKhoi nvarchar(10) not null,
+(	MaLop nvarchar(20) not null,
+	TenLop nvarchar(30),
+	MaKhoi nvarchar(20) not null,
 	SiSo tinyint,
-	MaNamHoc nvarchar(10) not null,
+	MaNamHoc nvarchar(20) not null,
 	constraint pk_ml primary key(MaLop),
 )
 
 CREATE TABLE HOCKY (
-	MaHocKy nvarchar(10),
-	HocKy nvarchar(10),
+	MaHocKy nvarchar(20),
+	HocKy nvarchar(20),
 	TrongSo float,
 	constraint pk_mhk primary key(MaHocKy)
 )
 
 CREATE TABLE MONHOC
-(	MaMonHoc nvarchar(10) not null,
+(	MaMonHoc nvarchar(20) not null,
 	TenMonHoc nvarchar(50),
 	constraint pk_mmh primary key(MaMonHoc)
 )
 
 CREATE TABLE CTLOP
-(	MaCTL nvarchar(10) not null,
-	MaHocSinh nvarchar(10) not null,
-	MaLop nvarchar(10) not null,
+(	MaCTL nvarchar(20) not null,
+	MaHocSinh nvarchar(20) not null,
+	MaLop nvarchar(20) not null,
 	constraint pk_ctl primary key(MaCTL)
 )
 
 CREATE TABLE DIEM
-(	MaDiem nvarchar(10) not null,
-	MaKetQua nvarchar(10) not null,
-	MaThanhPhan nvarchar(10),
+(	MaDiem nvarchar(20) not null,
+	MaKetQua nvarchar(20) not null,
+	MaThanhPhan nvarchar(20),
 	Diem float,
 	constraint pk_md primary key(MaDiem)
 )
 
 CREATE TABLE THANHPHAN (
-	MaThanhPhan nvarchar(10) not null,
+	MaThanhPhan nvarchar(20) not null,
 	TenThanhPhan nvarchar(30),
 	TrongSo float,
 	constraint pk_mtp primary key(MaThanhPhan)
 )
 
 CREATE TABLE KETQUA_MONHOC_HOCSINH (
-	MaKetQua nvarchar(10) not null,
-	MaHocSinh nvarchar(10) not null,
-	MaNamHoc nvarchar(10) not null,
-	MaHocKy nvarchar(10) not null,
-	MaMonHoc nvarchar(10) not null,
+	MaKetQua nvarchar(20) not null,
+	MaHocSinh nvarchar(20) not null,
+	MaNamHoc nvarchar(20) not null,
+	MaHocKy nvarchar(20) not null,
+	MaMonHoc nvarchar(20) not null,
 	DiemTB float,
-	MaXepLoai nvarchar(10)
+	MaXepLoai nvarchar(20)
 	constraint pk_mkq primary key(MaKetQua)
 )
 
 CREATE TABLE XEPLOAI
 (
-	MaXepLoai nvarchar (10) not null,
-	TenXepLoai nvarchar (20),
+	MaXepLoai nvarchar (20) not null,
+	TenXepLoai nvarchar (30),
 	DiemToiThieu float,
 	DiemToiDa float,
 	DiemKhongChe float, -- Ap dung khi xet Diem
@@ -107,13 +107,25 @@ CREATE TABLE XEPLOAI
 
 CREATE TABLE THAMSO
 (	
-	MaThamSo nvarchar (50) not null,
+	MaThamSo nvarchar (20) not null,
 	TuoiToiThieu tinyint default 15,
 	TuoiToiDa tinyint default 20,
 	SiSoToiDa smallint default 40,
 	DiemToiDaMH float default 10.0,
 	DiemToiThieu float default 0.0,
 	Constraint pk_ts primary key (MaThamSo)
+)
+
+CREATE TABLE PHANQUYEN (
+	MaPhanQuyen nvarchar(20) not null,
+	VaiTro nvarchar(50),
+	constraint pk_pq primary key(MaPhanQuyen)
+)
+
+CREATE TABLE TAIKHOAN (
+	MaTaiKhoan nvarchar(20) not null,
+	TenDangNhap nvarchar(60),
+	MatKhau nvarchar(60)
 )
 
 --TAO KHOA NGOAI
