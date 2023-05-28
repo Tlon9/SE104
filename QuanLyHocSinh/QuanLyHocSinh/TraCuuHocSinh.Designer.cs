1535f4332f0d06f7ab1454e184c9f8c34cf33430
Nguyen1ong <21520334@gm.uit.edu.vn>
2023-05-28 15:46:19 +0700

    Update TraCuuHocSinh Form

--- QuanLyHocSinh/QuanLyHocSinh/TraCuuHocSinh.Designer.cs
+++ QuanLyHocSinh/QuanLyHocSinh/TraCuuHocSinh.Designer.cs
@@ -747,13 +747,18 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.button1.Location = new System.Drawing.Point(871, 186);
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(871, 183);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 44);
+            this.button1.Size = new System.Drawing.Size(114, 44);
             this.button1.TabIndex = 9;
             this.button1.Text = "Nhập lại";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label31
