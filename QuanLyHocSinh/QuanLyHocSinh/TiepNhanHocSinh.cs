33900d78f10d5baf74fa79c0b02a03045c5c056e
PamumanTan <96417133+PamumanTan@users.noreply.github.com>
2023-06-11 15:38:03 +0700

    Sửa form Tiếp nhận học sinh và Lập danh sách lớp

--- QuanLyHocSinh/QuanLyHocSinh/TiepNhanHocSinh.cs
+++ QuanLyHocSinh/QuanLyHocSinh/TiepNhanHocSinh.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -54,6 +55,22 @@ namespace QuanLyHocSinh
         private void Btn_Close_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr one, int two, int three, int four);
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(Handle, 0x112, 0xf012, 0);
+        }
+
+        private void mainLabelStdInfo_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(Handle, 0x112, 0xf012, 0);
         }
     }
 }
