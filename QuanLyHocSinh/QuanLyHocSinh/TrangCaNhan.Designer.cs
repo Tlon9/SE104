27d56d530612a31d156b6ecd81b105b464d6c5fa
Tlon9 <21520331@gm.uit.edu.vn>
2023-06-12 12:00:16 +0700

    update TrangCaNhanForm

--- QuanLyHocSinh/QuanLyHocSinh/TrangCaNhan.Designer.cs
+++ QuanLyHocSinh/QuanLyHocSinh/TrangCaNhan.Designer.cs
@@ -120,7 +120,7 @@ namespace QuanLyHocSinh
             this.label2.Location = new System.Drawing.Point(13, 384);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(116, 28);
+            this.label2.Size = new System.Drawing.Size(170, 28);
             this.label2.TabIndex = 22;
             this.label2.Text = "Ngày sinh:";
             // 
